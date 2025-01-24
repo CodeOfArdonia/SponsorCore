134955b3b6d4b3a8defadbf8c7c09699b261e16d
IAFEnvoy <tanksheng545@126.com>
2025-01-24 16:03:12 +0800

    fix 1.20.4 neoforge

--- neoforge/src/main/java/com/iafenvoy/sponsor/neoforge/SponsorCoreNeoForge.java
+++ neoforge/src/main/java/com/iafenvoy/sponsor/neoforge/SponsorCoreNeoForge.java
@@ -1,11 +1,11 @@
-package com.iafenvoy.sponsor.forge;
+package com.iafenvoy.sponsor.neoforge;
 
 import com.iafenvoy.sponsor.SponsorCore;
 import net.neoforged.fml.common.Mod;
 
 @Mod(SponsorCore.MOD_ID)
-public final class SponsorCoreForge {
-    public SponsorCoreForge() {
+public final class SponsorCoreNeoForge {
+    public SponsorCoreNeoForge() {
         SponsorCore.init();
     }
 }
